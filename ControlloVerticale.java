3a3958c832514df5e08b2b2aeebeff5629d4e845
manez <160124076+ilmanez@users.noreply.github.com>
2024-05-09 11:51:07 +0200

    ControlloVerticale.java

--- ControlloVerticale.java
+++ ControlloVerticale.java
@@ -4,7 +4,9 @@ public class ControlloVerticale {
     private static boolean controlloVerticale(int giocatore) {
         for(int i=0; i< COLONNE; i++){
             for (int j=0; j<RIGHE-3; j++){
-                
+                if (griglia[j][i]== giocatore && griglia[j+1][i] && griglia [j+2][i]== giocatore && griglia[j+3][i] == giocatore){
+                    
+                }
             }
         }
     }
