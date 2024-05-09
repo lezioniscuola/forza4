b7cc8b61dc42460355e3db72fdf7cd667a06afc0
manez <160124076+ilmanez@users.noreply.github.com>
2024-05-09 11:46:40 +0200

    ControlloVerticale.java

--- ControlloVerticale.java
+++ ControlloVerticale.java
@@ -1,5 +1,11 @@
 public class ControlloVerticale {
     public static void main(String[] args) {
-
+    }
+    private static boolean controlloVerticale(int giocatore) {
+        for(int i=0; i< COLONNE; i++){
+            for (int j=0; j<RIGHE-3; j++){
+                
+            }
+        }
     }
 }
