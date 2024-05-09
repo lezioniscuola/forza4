0b90acdfce7f4f322845cbac7fa20b994ceca9aa
LeonardoRoncelli <160123628+LeonardoRoncelli@users.noreply.github.com>
2024-05-09 19:11:37 +0200

    Add files via upload
    Aggiunta for controllo diagonale da destra a sinistra

---
+++ ControlloDiagonaleRoncelli.java
@@ -0,0 +1,13 @@
+public class ControlloDiagonale (int giocatore) {
+    public static void main(String[] args) {
+        for(int riga=0;riga<RIGHE-3;riga++){
+            for(int colonna=0;colonna<COLONNE-3;colonna++){
+                if(griglia[riga][colonna]==giocatore&&griglia[riga+1][colonna+1]==giocatore&&griglia[riga+2][colonna-2]==giocatore&&griglia[riga+3][colonna-3]==giocatore){
+                    return true;
+                }
+            }
+        }
+        for (int riga = 0; riga < RIGHE - 3; riga++) {
+            for (int colonna = 3; colonna < COLONNE; colonna++) {
+    }
+}
