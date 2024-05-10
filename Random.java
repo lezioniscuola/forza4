cd3b8a8f7ed0c7e0ff31551aa3b484611d43c837
Silvalore <160123568+Silvalore@users.noreply.github.com>
2024-05-10 12:32:15 +0200

    Update Random.java

--- Random.java
+++ Random.java
@@ -2,6 +2,9 @@ public class Random {
     public static void main(String[] args) {
  int numeroRandom= NumeriRandom();
             System.out.println("Numero casuale: " + numeroRandom);
+        int numeroRando= NumeriRandom2();
+            System.out.println("Numero casuale: " + numeroRandom);
+        
     }
     private static int NumeriRandom() {
             Random r= new Random();
