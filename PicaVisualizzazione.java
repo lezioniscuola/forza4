33bc217c417ef37e1e9c640d2ccd115f68ca9590
AlessandroPica <160123537+AlessandroPica@users.noreply.github.com>
2024-05-10 12:29:54 +0200

    PicaVisualizzazione.java

--- PicaVisualizzazione.java
+++ PicaVisualizzazione.java
@@ -6,4 +6,10 @@ private static void visualizzaGriglia() {
         }
         System.out.println("\\|");
         System.out.println("|\\|||||||||||||||||||||||||||||||||||||||||\\|");
+        for (int riga = 0; riga < RIGHE; riga++) {
+            System.out.print("|\\|");
+            for (int colonna = 0; colonna < COLONNE; colonna++) {
+                System.out.print(griglia[riga][colonna] == 0 ? "- -" : (griglia[riga][colonna] == 1 ? " X " : " O "));
+                System.out.print("|");
+            }
     }
