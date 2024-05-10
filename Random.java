4043ddc8334724f3436cc2e8e8fb6f3a30af0f9f
Silvalore <160123568+Silvalore@users.noreply.github.com>
2024-05-10 12:24:06 +0200

    Update Random.java

--- Random.java
+++ Random.java
@@ -5,4 +5,9 @@ public class Random {
     private static int NumeriRandom() {
             Random r= new Random();
             return r.nextInt(3); 
+    }
+    private static int NumeriRandom2() {
+            Random r2= new Random();
+            return r2.nextInt(10);
+    }
 }
