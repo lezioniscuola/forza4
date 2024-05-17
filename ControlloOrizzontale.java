3465a51cbb4575169604fdb1f84cb7c3e36d5e88
alxkndr <160123102+alxkndr@users.noreply.github.com>
2024-05-17 09:11:13 +0200

    Update ControlloOrizzontale.java
    ritorna vero se ha 4 coin sulla stessa linea orizzontale, quindi ha vinto, ritorna falso altrimenti

--- ControlloOrizzontale.java
+++ ControlloOrizzontale.java
@@ -4,4 +4,10 @@ for (int riga = 0; riga < RIGHE; riga++) {
             for (int colonna = 0; colonna < COLONNE - 3; colonna++) {
                         if (griglia[riga][colonna] == giocatore && griglia[riga][colonna + 1] == giocatore &&
                         griglia[riga][colonna + 2] == giocatore && griglia[riga][colonna + 3] == giocatore) {
+                                    return true;
+                                    }
+                        }
+            }
+            return false;
+            }
 }
