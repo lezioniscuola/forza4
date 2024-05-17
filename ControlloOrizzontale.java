f490609b2abdef49824cbb52221d8c02e5b61f78
alxkndr <160123102+alxkndr@users.noreply.github.com>
2024-05-17 12:15:48 +0200

    Update ControlloOrizzontale.java

--- ControlloOrizzontale.java
+++ ControlloOrizzontale.java
@@ -1,9 +1,7 @@
-private static boolean
-controlloOrizzontale(int giocatore) {
+private static boolean controlloOrizzontale(int giocatore) {
 for (int riga = 0; riga < RIGHE; riga++) {
             for (int colonna = 0; colonna < COLONNE - 3; colonna++) {
-                        if (griglia[riga][colonna] == giocatore && griglia[riga][colonna + 1] == giocatore &&
-                        griglia[riga][colonna + 2] == giocatore && griglia[riga][colonna + 3] == giocatore) {
+                        if (griglia[riga][colonna] == giocatore && griglia[riga][colonna + 1] == giocatore && griglia[riga][colonna + 2] == giocatore && griglia[riga][colonna + 3] == giocatore) {
                                     return true;
                                     }
                         }
