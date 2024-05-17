55746f571a65ec6f267ab2e094938ced7378bea4
alxkndr <160123102+alxkndr@users.noreply.github.com>
2024-05-17 09:09:13 +0200

    Update ControlloOrizzontale.java
    4 && per vincere il gioco

--- ControlloOrizzontale.java
+++ ControlloOrizzontale.java
@@ -2,5 +2,6 @@ private static boolean
 controlloOrizzontale(int giocatore) {
 for (int riga = 0; riga < RIGHE; riga++) {
             for (int colonna = 0; colonna < COLONNE - 3; colonna++) {
-                        if (griglia[riga][colonna] == giocatore
+                        if (griglia[riga][colonna] == giocatore && griglia[riga][colonna + 1] == giocatore &&
+                        griglia[riga][colonna + 2] == giocatore && griglia[riga][colonna + 3] == giocatore) {
 }
